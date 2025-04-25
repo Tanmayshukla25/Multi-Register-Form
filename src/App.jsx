c62f5845d80c7a66f9cf9260a96ba7b14461c8b8
tanmay <tanmayshukla252@gmail.com>
2025-04-25 11:07:10 +0530

    changes in code

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ function App() {
   function SigninBtn() {
     if (pass.value.length < 6) {
       alert("Please enter atleast 6 digit password ");
-    } else if (confpass.value.length < 6) {
-      alert("Please enter atleast 6 digit password");
+    } else if (confpass.value.lenght === pass.value.length) {
+      alert("Please enter currect password");
     } else if (phone.value.length < 10) {
       alert("Please enter atleast 10 digit Number ");
     } else if (pass.value !== confpass.value) {
