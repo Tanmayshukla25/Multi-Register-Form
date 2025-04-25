000e87f43e84bbae9107fe3ca3c7ba5d1cc3fc39
tanmay <tanmayshukla252@gmail.com>
2025-04-25 11:03:48 +0530

    addConditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Header from "./Header";
 import contactimg from "./images/contactimg.png";
 import loginimg from "./images/loginimg-removebg-preview.png";
@@ -10,9 +10,7 @@ function App() {
   const [step, setStep] = useState(0);
 
   const nextStep = () => {
-    if (step < steps.length) 
-      setStep(step + 1);
-
+    if (step < steps.length) setStep(step + 1);
   };
 
   const prevStep = () => {
@@ -21,88 +19,114 @@ function App() {
     }
   };
 
-    
+  function SigninBtn() {
+    if (pass.value.length < 6) {
+      alert("Please enter atleast 6 digit password ");
+    } else if (confpass.value.length < 6) {
+      alert("Please enter atleast 6 digit password");
+    } else if (phone.value.length < 10) {
+      alert("Please enter atleast 10 digit Number ");
+    } else if (pass.value !== confpass.value) {
+      alert("Password are not same");
+    }
+  }
+
   const steps = [
     <div key={1}>
-    <div id='HomePage'>
-      <div id='head'>
-        <h1>Login & Registration Form</h1>
+      <div id="HomePage">
+        <div id="head">
+          <h1>Login & Registration Form</h1>
+        </div>
+        <div id="InputDiv">
+          <h1>Sign-Up</h1>
+          <form action="">
+            <input type="text" id="first" placeholder="Username" required />
+            <br />
+            <input type="Email" id="email" placeholder="Email Id" required />
+            <br />
+            <input type="Password" id="pass" placeholder=" Password" required />
+            <br />
+            <input
+              type="Password"
+              id="confpass"
+              placeholder=" Confirm-Password"
+              required
+            />
+            <br />
+            <button id="btnaccount" onClick={SigninBtn}>
+              Sign-In
+            </button>
+          </form>
+        </div>
       </div>
-      <div id='InputDiv'>
-        <h1>Sign-Up</h1>
-        <form action="">
-          <input type="text" placeholder='Username' required/><br />
-          <input type="Email" placeholder='Email Id' required/><br />
-          <input type="Password" placeholder=' Password' required/><br />
-          <input type="Password" placeholder=' Confirm-Password' required/><br />
-          <button id='btnaccount'>Sign-In</button>
-        </form>
-      </div>
-
-    </div>
     </div>,
 
     <div key={2}>
-      <div id='HomePage'>
-      <div id='contactpage'>
-      <img src={contactimg} alt="" />
-      </div>
-      <div id='InputDiv2'>
-        <h1>Contact-Us</h1>
-        <form action="">
-          <input type="Number" placeholder='Phone Number' required/><br />
-          <input type="Address" placeholder='Address' required/><br />
-          <input type="text" id='messagebox' placeholder='Message' required/><br />
-         
-          <button id='btnaccount'>Create-Account.</button>
-        </form>
-      </div>
+      <div id="HomePage">
+        <div id="contactpage">
+          <img src={contactimg} alt="" />
+        </div>
+        <div id="InputDiv2">
+          <h1>Contact-Us</h1>
+          <form action="">
+            <input
+              type="Number"
+              id="phone"
+              placeholder="Phone Number"
+              required
+            />
+            <br />
+            <input type="Address" placeholder="Address" required />
+            <br />
+            <input type="text" id="messagebox" placeholder="Message" required />
+            <br />
 
-    </div>
+            <button id="btnaccount" onClick={SigninBtn}>
+              Create-Account.
+            </button>
+          </form>
+        </div>
+      </div>
     </div>,
 
     <div key={3}>
-        <div id='HomePage'>
-      <div id='contactpage'>
-      <img src={loginimg} alt="" />
+      <div id="HomePage">
+        <div id="contactpage">
+          <img src={loginimg} alt="" />
+        </div>
+        <div id="InputDiv3">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username" required />
+            <br />
+            <input type="Password" placeholder="Password" required />
+            <br />
+            <button id="btnaccount" onClick={SigninBtn}>
+              Register
+            </button>
+            <p>
+              Forget <span id="forget">Username/password</span>?
+            </p>
+          </form>
+        </div>
       </div>
-      <div id='InputDiv3'>
-        <h1>Login</h1>
-        <form action="">
-          <input type="text" placeholder='Username' required/><br />
-          <input type="Password" placeholder='Password'required/><br />
-          <button id='btnaccount'>Register</button>
-          <p>Forget <span id='forget'>Username/password</span>?</p>
-        </form>
-      </div>
-
-    </div>
-    </div>
-
-   
+    </div>,
   ];
 
- 
   return (
     <>
-       <Header />
-    
-     
-   <div id='container'>
-   <div id="form-container">
-        
-       
-{steps[step]}
-   
-     
+      <Header />
 
-      <div>
-     
-        {step > 0 && <FaArrowLeft  onClick={prevStep} id='arrowleft'/>}
-        {step < 2 && <FaArrowRight onClick={nextStep} id='arrowRight' />}
+      <div id="container">
+        <div id="form-container">
+          {steps[step]}
+
+          <div>
+            {step > 0 && <FaArrowLeft onClick={prevStep} id="arrowleft" />}
+            {step < 2 && <FaArrowRight onClick={nextStep} id="arrowRight" />}
+          </div>
+        </div>
       </div>
-      </div>
-   </div>
     </>
   );
 }
